b21f5f76da353e1044b31d9c557284f169d47400
Redouane DRAYEF <redouane_drayef@yahoo.com>
2023-12-16 19:43:53 +0100

    Project setup

--- cloudifier-frontend/src/App.js
+++ cloudifier-frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Menu from './components/Menu/Menu';
+import MachinesPage from './pages/MachinesPage';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+          <div>
+            <Menu />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/machines" component={MachinesPage} />
+            </Switch>
+          </div>
+        </Router>
+      );
 }
 
-export default App;
+export default App
